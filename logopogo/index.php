18f909d785ce0e3fa629cdd3cc903cbbd5c20bca
Chris Lewis <clewis@typenetwork.com>
2017-09-13 17:11:24 +0000

    Remove PDF/TTF download options for logopogo

--- logopogo/index.php
+++ logopogo/index.php
@@ -30,10 +30,12 @@ print $tools->pageHead('Logo Pogo')
 			Brand Name
 		</figure>
 		<div id='save-buttons'>
-			<h3>Save as:</h3>
-			<button class="button_default" id='save-svg'>SVG</button>
+<!-- 			<h3>Save as:</h3> -->
+			<button class="button_default" id='save-svg'>Download SVG</button>
+<!--
 			<button class="button_default" id='save-pdf'>PDF</button>
 			<button class="button_default" id='save-ttf'>TTF</button>
+-->
 		</div>
 		<div id='css-output'></div>
 	</body>
