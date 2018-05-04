f53815355532ee55f4633dad30ed5d9aabb2afee
Chris Lewis <clewis@typenetwork.com>
2018-05-04 17:34:59 +0000

    Simplify CSS structure for multi-paragraph styles

--- typespec/typespec.php
+++ typespec/typespec.php
@@ -10,25 +10,25 @@ $tools = new TypeTools();
 <article spellcheck="false">
 	<div class="H1 row">
 		<label>H1</label>
-		<h1 contenteditable>Heading One</h1>
+		<h1 class='H1-target' contenteditable>Heading One</h1>
 	</div>
 	<div class="H2 row">
 		<label>H2</label>
-		<h2 contenteditable>Heading Two</h2>
+		<h2 class='H2-target' contenteditable>Heading Two</h2>
 	</div>
 	<div class="H3 row">
 		<label>H3</label>
-		<h3 contenteditable>Heading Three</h3>
+		<h3 class='H3-target' contenteditable>Heading Three</h3>
 	</div>
 	<div class="T1 row">
 		<label>T1</label>
-		<p class="lede" contenteditable>
+		<p class="lede T1-target" contenteditable>
 			Intro text leads reader into the article by the nose, with grace and dignity and a little pithy charm. Typeface has changed to the appropriate optical size by the miracle of modern typography.
 		</p>
 	</div>
 	<div class="T2 row">
 		<label>T2</label>
-		<div class="container" contenteditable>
+		<div class="T2-target" contenteditable>
 			<p>Johannes Gutenberg’s work on the printing press began in approximately 1436 when he partnered with Andreas Heilmann, owner of a paper mill. Having previously worked as a goldsmith, Gutenberg made skillful use of the knowledge of metals he had learned as a craftsman. He was the first to make type from an alloy of lead, tin, and antimony, which was critical for producing durable type that produced high-quality printed books and proved to be much better suited for printing than all other known materials.
 			</p>
 			<p>The introduction of mechanical movable type printing led to a huge increase of printing activities across Europe within only a few decades. From a single print shop in Mainz, Germany, printing had spread to no less than around 270 cities in Central, Western and Eastern Europe by the end of the 15th century. As early as 1480, there were printers active in 110 different places in Germany, Italy, France, Spain, the Netherlands, Belgium, Switzerland, England, Bohemia and Poland. From that time on, it is assumed that “the printed book was in universal use in Europe.”
